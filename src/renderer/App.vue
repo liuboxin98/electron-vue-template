df473014bedb9912d3ae28e2520577d15eeb5ed5
liubx <liu6email@163.com>
2023-04-26 09:43:38 +0800

    init

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,32 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
-window.electronAPI.sendMessage('Hello from App.vue 2023!');
+window.electronAPI.sendMessage("Hello from App.vue 2023!");
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <HelloWorld msg="Vite + Vue" />
+    <hr />
+    <div class="wrapper">
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/count">Count</RouterLink>
+        </nav>
+    </div>
+    <hr />
+    <RouterView />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.wrapper nav a {
+    margin: 0 10px;
 }
 </style>
---
+++ src/renderer/views/CountView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useCounterStore } from "../stores";
+
+const counterStore = useCounterStore();
+</script>
+
+<template>
+    <h1>{{ counterStore.count }}</h1>
+    <h1>x2 {{ counterStore.doubleCount }}</h1>
+
+    <button @click="counterStore.increment()">add</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
