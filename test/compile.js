54818de80fe2aad64e652aa2623bbe7d5a75efb4
liubx <liu6email@163.com>
2024-08-08 14:03:19 +0800

    chore: update config

--- test/compile.js
+++ test/compile.js
@@ -1,12 +1,13 @@
 const ChildProcess = require('child_process');
 const Chalk = require('chalk');
 const Path = require('path');
+const FileSystem = require('fs');
 
 
 function compile(directory) {
     return new Promise((resolve, reject) => {
 
-        const outputPath = Path.resolve(directory, '../../test/finally');
+        const outputPath = Path.resolve(directory, '../finally');
         console.log(`Compiling files from ${directory} to ${outputPath}`);
 
         const babelCommand = [
@@ -14,26 +15,27 @@ function compile(directory) {
             directory,
             `--out-dir ${outputPath}`,
             '--ignore static',
-            '--presets @babel/preset-env'
+            '--presets @babel/preset-env',
+            '--presets minify',
         ].join(' ');
 
         const babelProcess = ChildProcess.exec(babelCommand, {
             cwd: directory,
         });
 
-		let errorMessage = '';
+        let errorMessage = '';
 
         babelProcess.stdout.on('data', data =>
             process.stdout.write(Chalk.yellowBright(`[babel] `) + Chalk.white(data.toString()))
         );
 
         babelProcess.stderr.on('data', data => {
-			errorMessage += data.toString();
+            errorMessage += data.toString();
         });
-        
+
         babelProcess.on('exit', exitCode => {
             if (exitCode > 0) {
-				reject({ exitCode, message: errorMessage });
+                reject({ exitCode, message: errorMessage });
             } else {
                 resolve();
             }
@@ -43,15 +45,29 @@ function compile(directory) {
 }
 
 function start() {
-    const sourcePath = Path.join(__dirname, '..', 'test', 'source');
+    const sourcePath = Path.join(__dirname, 'source');
     compile(sourcePath)
         .then(() => {
+            copy('static');
+
             console.log('Compilation complete');
         })
         .catch(error => {
-			console.error(`Compilation failed with exit code ${error.exitCode}: ${error.message}`);
+            console.error(`Compilation failed with exit code ${error.exitCode}: ${error.message}`);
         });
 }
 
+function copy(path) {
+    FileSystem.cpSync(
+        Path.join(__dirname, 'source', path),
+        Path.join(__dirname, 'finally', path),
+        { recursive: true }
+    );
+}
+
+FileSystem.rmSync(Path.join(__dirname, '..', 'test', 'finally'), {
+    recursive: true,
+    force: true,
+})
 
 start();
--- test/finally/test1.js
+++ test/finally/test1.js
@@ -1,10 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _path = require("path");
-var mainPath = _path.Path.join(__dirname, '..', 'src', 'main');
-console.log(mainPath);
-var _default = exports["default"] = mainPath;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports["default"]=void 0;var _path=require("path"),mainPath=_path.Path.join(__dirname,"..","src","main");console.log(mainPath);var _default=exports["default"]=mainPath;
